e2b8bdfe58a4284fe5b5b349b35fcc6012797bc1
Nishieeee <pagaroganjhonclein@gmail.com>
2026-05-20 19:41:39 +0800

    feat: gemini api waste image preparation recommendation

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ResourceController;
 
+use App\Http\Controllers\WasteAssessmentController;
+
 Route::apiResource('resources',ResourceController::class);
+Route::post('/assess-waste', [WasteAssessmentController::class, 'assess']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
